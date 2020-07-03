78114b1a8cdd56a89340430be2995f0a3ab6d8ab
soorajphilip <surajphilip001@gmail.com>
2020-07-03 17:33:50 +0530

    .

--- test.rb
+++ test.rb
@@ -1,8 +1,6 @@
-x = gets.chop.to_i
-if x == 10
-  puts "number is 10"
-elsif x == 5
-  puts "number is 5"
-else
-  puts "incorrect number"
+grade = gets.chop
+di_i_pass = case grade
+when 'A' then puts 'hell yeah'
+when 'D' then puts 'dont tell your mother'
+else puts 'bad'
 end
