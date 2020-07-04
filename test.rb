3a3552f110eb1b92ab59245f41666b0009277cdd
soorajphilip <surajphilip001@gmail.com>
2020-07-04 17:29:38 +0530

    l

--- test.rb
+++ test.rb
@@ -1,10 +1,7 @@
-a = gets.chop.to_i
-ans = case a
-when 5
-  "ans is 5"
-when 6
-  "ans is 6"
-else
-  "a is neither 5 nor 6"
+def add_value(&add)
+  add.call
 end
-puts ans
+
+add_value do
+  puts "plus operator"
+end
