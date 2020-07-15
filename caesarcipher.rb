bbee1328c95cda54bd945ef48cd84e5b7e3bf29d
soorajphilip <surajphilip001@gmail.com>
2020-07-15 12:50:32 +0530

    rails folders

--- caesarcipher.rb
+++ caesarcipher.rb
@@ -1,26 +1,26 @@
+require "pry"
 # caeser cipher encryption
 puts "Enter string: "
 string = gets.chomp
-puts "Enter number for encryption: "
-number = gets.chomp.to_i
 
+# puts "Enter number for encryption: "
+# number = gets.chomp.to_i
+
+number = Random.rand(1..10)
 array1 = []
 x = string.split
 array1 << x
 a1 = array1.flatten
-
 array2 = []
 a1.each do |str|
   array2 << str.split("")
 end
 a2 = array2
-
 array3 = []
-("a".."z").each do |char|
+"a".upto("z") do |char|
   array3 << char
 end
 a3 = array3
-
 array4 = []
 for i in a2
   for j in i
@@ -28,19 +28,26 @@ for i in a2
   end
 end
 a4 = array4
-
 array5 = []
 a4.map do |num|
   array5 << num + number
 end
 a5 = array5
-
+g = []
+a5.map do |elem|
+  if elem > 26
+    g << elem - 26
+  else
+    g << elem
+  end
+end
 array6 = []
-a5.each do |cn|
+g.each do |cn|
   array6 << a3[cn]
 end
 a6 = array6
 r = a2.join("-")
 t = a6.join("-")
 puts "input msg => #{r}"
+puts "number used for encryption: #{number}"
 puts "encrypted msg => #{t}"
