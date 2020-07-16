68093ffa4df548bf75092a1f709ff5f83d861486
soorajphilip <surajphilip001@gmail.com>
2020-07-16 18:08:04 +0530

    classes and objects

--- test.rb
+++ test.rb
@@ -1,2 +1,62 @@
-x = Random.rand(10)
-puts x
+# # => classes and object
+#
+# class Book
+#   attr_accessor :title, :author, :pages
+# end
+#
+# book1 = Book.new()
+# book1.title = "Harry potter"
+# book1.author = "jk rowling"
+# book1.pages = 400
+#
+# book2 = Book.new()
+# book2.title = "lord of the rings"
+# book2.author = "Tolkein"
+# book2.pages = 300
+#
+# book3 = Book.new()
+# book3.title = "Goosbumps"
+# book3.author = "vrubnin"
+# book3.pages = 500
+#
+# puts book1.title
+# puts book2.author
+# puts book3.pages
+
+# # => initialize method
+# class Book
+#   attr_accessor :title, :author, :pages
+#   def initialize(title, author, pages)
+#     @title = title
+#     @author = author
+#     @pages = pages
+#   end
+# end
+#
+# book1 = Book.new("harry potter","jk rowling",400)
+# book2 = Book.new("lord of the rings","tolkeins",300)
+#
+# puts book1.title
+# puts book2.author
+
+# # => instance methods
+# class Student
+#   attr_accessor :name, :rollnum, :gpa
+#   def initialize(name,rollnum,gpa)
+#     @name = name
+#     @rollnum = rollnum
+#     @gpa = gpa
+#   end
+#
+#   def has_honors
+#     if @gpa >= 5
+#       return true
+#     end
+#     return false
+#   end
+# end
+#
+# student1 = Student.new("sooraj",22,4.5)
+# student2 = Student.new("ryan",23,5.6)
+#
+# puts student2.has_honors
--- test1.rb
+++ test1.rb
@@ -1,2 +1,18 @@
-name = "sooraj"
-puts name[0,2]
+class Child
+  @@children = 0
+  def initialize(name, birth_year)
+    @name = name
+    @birth_year = birth_year
+    @@children +=1
+  end
+
+  def self.children_added
+    return @@children
+  end
+
+end
+
+naomi = Child.new("Naomi", 2006)
+bertha = Child.new("Bertha", 2008)
+
+puts Child.children_added # => 2
