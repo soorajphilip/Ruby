f35135df7420e1ddfb4cafb62bb902e2c102a41d
soorajphilip <surajphilip001@gmail.com>
2020-08-07 13:02:32 +0530

    new changes

--- test.rb
+++ test.rb
@@ -1,79 +1,67 @@
-# # => classes and object
-#
-# class Book
-#   attr_accessor :title, :author, :pages
+require "pry"
+# def add_nums(nums)
+# 	result = 0
+# 	array = nums.split(",")
+# 	array.each do |num|
+# 		result += num.to_i
+# 	end
+# 	puts result
 # end
-#
-# book1 = Book.new()
-# book1.title = "Harry potter"
-# book1.author = "jk rowling"
-# book1.pages = 400
-#
-# book2 = Book.new()
-# book2.title = "lord of the rings"
-# book2.author = "Tolkein"
-# book2.pages = 300
-#
-# book3 = Book.new()
-# book3.title = "Goosbumps"
-# book3.author = "vrubnin"
-# book3.pages = 500
-#
-# puts book1.title
-# puts book2.author
-# puts book3.pages
 
-# # => initialize method
-# class Book
-#   attr_accessor :title, :author, :pages
-#   def initialize(title, author, pages)
-#     @title = title
-#     @author = author
-#     @pages = pages
-#   end
-# end
-#
-# book1 = Book.new("harry potter","jk rowling",400)
-# book2 = Book.new("lord of the rings","tolkeins",300)
-#
-# puts book1.title
-# puts book2.author
+# add_nums("1,2,3,4,5")
 
-# # => instance methods
-# class Student
-#   attr_accessor :name, :rollnum, :gpa
-#   def initialize(name,rollnum,gpa)
-#     @name = name
-#     @rollnum = rollnum
-#     @gpa = gpa
-#   end
-#
-#   def has_honors
-#     if @gpa >= 5
-#       return true
-#     end
-#     return false
-#   end
+# def add_nums(nums)
+# 	array = nums.split(",")
+# 	array.map { |num| num.to_i }.sum
 # end
-#
-# student1 = Student.new("sooraj",22,4.5)
-# student2 = Student.new("ryan",23,5.6)
-#
-# puts student2.has_honors
+# puts add_nums("2, 5, 1, 8, 4")
 
-# # => modules
-#
-# module Tools
-#
-#   def sayhi(name)
-#     puts "hello, #{name}"
-#   end
-#   def saybye(name)
-#     puts "bye,#{name}"
-#   end
+# def add_nums(nums)
+# 	nums.split(", ").map { |num| num.to_i }.reduce(:+)
 # end
-#
-# include Tools
-#
-# Tools.sayhi("sooraj")
-# Tools.saybye("sooraj")
+
+# puts add_nums("2, 5, 1, 8, 4")
+# 
+
+# future_bank_account = 0
+# grade = gets.chomp
+# case grade
+# when 'A'
+# 	puts "you are a genius"
+# 	future_bank_account += 5000000
+# when 'D'
+# 	puts "better luck nxt time"
+# 	can_i_retire_soon = false
+# else
+# 	puts "mcdonalds is hiring"
+# 	fml = true
+# end
+
+
+# words = ["demo","edmo","mode","dad","add","dda"]
+# result = {}
+# words.each do |word|
+# 	key = word.split('').sort.join
+# 	if result.has_key?(key)
+# 		result[key].push(word)
+# 	else
+# 		result[key] = [word]
+# 	end
+# end
+# binding.pry
+# result.each_value do |v|
+# 	puts "------"
+# 	p v 
+# end
+# 
+
+def caesar(a,b)
+  	letters = Array('a'..'z').join
+    encrypter = letters.chars.rotate(b).join
+    a.tr(letters, encrypter)
+end
+puts "Enter the text you wanna encrypt below"
+a = gets.chomp
+puts "Enter the shift number below"
+b = gets.chomp.to_i
+p caesar(a,b)
